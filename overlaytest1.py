ad8094de0be961bccf937e4bfe2fd019557c1163
Awesteng <ang@lyft.com>
2019-01-17 08:48:38 -0800

    test

--- overlaytest1.py
+++ overlaytest1.py
@@ -3,6 +3,7 @@ from PIL import Image
 from time import sleep
 from gpiozero import Button
 
+
 camera = picamera.PiCamera()
 camera.resolution = (1280, 720)
 camera.framerate = 24
@@ -37,6 +38,6 @@ def overlay():
 # Wait indefinitely until the user terminates the script
     while True:
         sleep(1)
-#while True:
-    #button.when_pressed = overlay()
-    #button.when_released = camera.start_preview()
+while True:
+    button.when_pressed = overlay()
+    
