b0b799ae85b282b7970c1e51ead9fef5acfee6f3
Awesteng <ang@lyft.com>
2019-01-16 16:33:17 -0800

    edit

--- video.py
+++ video.py
@@ -1,7 +1,9 @@
-from picamera import PiCamera
+import PiCamera
 
 camera = picamera.PiCamera()
 camera.resolution = (1920, 1080)
+camera.start_preview()
 camera.start_recording('my_video.h264')
 camera.wait_recording(30)
 camera.stop_recording()
+camera.stop_preview()
