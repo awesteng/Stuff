ca68aaeb6c888795ff03e64c966c607e5204765f
Awesteng <ang@lyft.com>
2019-01-22 08:09:17 -0800

    test

--- overlaytest1.py
+++ overlaytest1.py
@@ -3,46 +3,39 @@ from PIL import Image
 from time import sleep
 from gpiozero import Button
 
-
 camera = picamera.PiCamera()
 camera.resolution = (1280, 720)
 camera.framerate = 24
 button = Button(2)
 camera.start_preview()
 
+button.wait_for_press()
 
 
-def overlay():
-
-  img = Image.open('CFoa1.gif')
-  Image.framerate = 4
+# Load the arbitrarily sized image
+img = Image.open('CFoa1.gif')
+Image.framerate = 4
 # Create an image padded to the required size with
 # mode 'RGB'
-  pad = Image.new('RGB', (
-      ((img.size[0] + 31) // 32) * 32,
-      ((img.size[1] + 15) // 16) * 16,
-      ))
+pad = Image.new('RGB', (
+    ((img.size[0] + 31) // 32) * 32,
+    ((img.size[1] + 15) // 16) * 16,
+    ))
 # Paste the original image into the padded one
-  pad.paste(img, (0, 0))
+pad.paste(img, (0, 0))
 
 # Add the overlay with the padded image as the source,
 # but the original image's dimensions
-  o = camera.add_overlay(pad.tobytes(), size=img.size)
+o = camera.add_overlay(pad.tobytes(), size=img.size)
 # By default, the overlay is in layer 0, beneath the
 # preview (which defaults to layer 2). Here we make
 # the new overlay semi-transparent, then move it above
 # the preview
-  o.alpha = 128
-  o.layer = 3
+o.alpha = 128
+o.layer = 3
 
-  while True:
-      sleep(1)
+# Wait indefinitely until the user terminates the script
+while True:
+    sleep(1)
 
-def default():
-    Image.close()
-
-
-if button.when_pressed():
-    overlay()
-else button.when_released():
-    default()
+button.when_released() Image.close('Cfoa1.gif')
