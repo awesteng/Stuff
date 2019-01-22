ff2a32f4c9028807e178acfa2558c63a47c836d4
Awesteng <ang@lyft.com>
2019-01-22 08:19:12 -0800

    test

--- overlaytest1.py
+++ overlaytest1.py
@@ -38,5 +38,32 @@ def overlay():
     while True:
         sleep(1)
 
+def default():
+# Load the arbitrarily sized image
+    img = Image.close('CFoa1.gif')
+    Image.framerate = 4
+# Create an image padded to the required size with
+# mode 'RGB'
+    pad = Image.new('RGB', (
+        ((img.size[0] + 31) // 32) * 32,
+        ((img.size[1] + 15) // 16) * 16,
+        ))
+# Paste the original image into the padded one
+    pad.paste(img, (0, 0))
+
+# Add the overlay with the padded image as the source,
+# but the original image's dimensions
+    o = camera.add_overlay(pad.tobytes(), size=img.size)
+# By default, the overlay is in layer 0, beneath the
+# preview (which defaults to layer 2). Here we make
+# the new overlay semi-transparent, then move it above
+# the preview
+    o.alpha = 128
+    o.layer = 3
+
+# Wait indefinitely until the user terminates the script
+    while True:
+        sleep(1)
+
 button.when_pressed = overlay()
-button.when_released = Image.close('Cfoa1.gif')
+button.when_released = default()
